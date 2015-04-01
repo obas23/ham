859bc1fabd3d349b9ddb94fbc23d9d0c58dc0083
Casey O'Hara <casey.ohara@me.com>
2015-03-31 20:16:30 -0600

    Update JSON schema to support Ember

--- lib/ham/web/api.rb
+++ lib/ham/web/api.rb
@@ -31,45 +31,45 @@ module Ham
 
       get "/gifs" do
         gifs = Gif.search(params[:q])
-        gifs.to_json
+        {gifs: gifs}.to_json
       end
 
       post "/gifs" do
         status 201
         gif = Gif.create(params[:gif])
-        gif.to_json
+        {gif: gif}.to_json
       end
 
       get "/gifs/:gif" do
         gif = Gif.retrieve(params[:gif])
-        gif.to_json
+        {gif: gif}.to_json
       end
 
       get "/gifs/:gif/tags" do
         gif = Gif.retrieve(params[:gif])
-        gif.tags.to_json
+        {tags: gif.tags}.to_json
       end
 
       post "/gifs/:gif/tags" do
         status 201
         tag = Gif.tag(params[:gif], params[:tag])
-        tag.to_json
+        {tag: tag}.to_json
       end
 
       delete "/gifs/:gif/tags/:tag" do
         status 202
         tag = Gif.untag(params[:gif], params[:tag])
-        tag.to_json
+        {tag: tag}.to_json
       end
 
       get "/tags" do
         tags = Tag.search(params[:q])
-        tags.to_json
+        {tags: tags}.to_json
       end
 
       get "/tags/complete" do
         tags = Tag.complete(params[:q])
-        tags.to_json
+        {tags: tags}.to_json
       end
     end
   end
--- test.rb
+++ test.rb
@@ -30,7 +30,9 @@ Shakedown.sequence do
 
   get "/gifs" do
     status 200
-    []
+    {
+      gifs: []
+    }
   end
 
   get "/gifs/gif1" do
@@ -43,189 +45,230 @@ Shakedown.sequence do
   post "/gifs", { gif: "gif1" } do
     status 201
     {
-      id: "gif1",
-      url: "http://i.imgur.com/gif1.gif",
-      thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      gif: {
+        id: "gif1",
+        url: "http://i.imgur.com/gif1.gif",
+        thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      }
     }
   end
 
   get "/gifs" do
     status 200
-    [{
-      id: "gif1",
-      url: "http://i.imgur.com/gif1.gif",
-      thumbnail_url: "http://i.imgur.com/gif1b.gif"
-    }]
+    {
+      gifs: [{
+        id: "gif1",
+        url: "http://i.imgur.com/gif1.gif",
+        thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      }]
+    }
   end
 
   get "/gifs/gif1" do
     status 200
     {
-      id: "gif1",
-      url: "http://i.imgur.com/gif1.gif",
-      thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      gif: {
+        id: "gif1",
+        url: "http://i.imgur.com/gif1.gif",
+        thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      }
     }
   end
 
   post "/gifs", { gif: "gif2" } do
     status 201
     {
-      id: "gif2",
-      url: "http://i.imgur.com/gif2.gif",
-      thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      gif: {
+        id: "gif2",
+        url: "http://i.imgur.com/gif2.gif",
+        thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      }
     }
   end
 
   get "/gifs" do
     status 200
-    [{
-      id: "gif2",
-      url: "http://i.imgur.com/gif2.gif",
-      thumbnail_url: "http://i.imgur.com/gif2b.gif"
-    }, {
-      id: "gif1",
-      url: "http://i.imgur.com/gif1.gif",
-      thumbnail_url: "http://i.imgur.com/gif1b.gif"
-    }]
+    {
+      gifs: [{
+        id: "gif2",
+        url: "http://i.imgur.com/gif2.gif",
+        thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      }, {
+        id: "gif1",
+        url: "http://i.imgur.com/gif1.gif",
+        thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      }]
+    }
   end
 
   get "/gifs/gif2" do
     status 200
     {
-      id: "gif2",
-      url: "http://i.imgur.com/gif2.gif",
-      thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      gif: {
+        id: "gif2",
+        url: "http://i.imgur.com/gif2.gif",
+        thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      }
     }
   end
 
   get "/tags" do
     status 200
-    []
+    {
+      tags: []
+    }
   end
 
   post "/gifs/gif1/tags", { tag: "Shared Tag" } do
     status 201
     {
-      id: "shared-tag",
-      text: "shared tag"
+      tag: {
+        id: "shared-tag",
+        text: "shared tag"
+      }
     }
   end
 
   post "/gifs/gif1/tags", { tag: "Custom Tag 1" } do
     status 201
     {
-      id: "custom-tag-1",
-      text: "custom tag 1"
+      tag: {
+        id: "custom-tag-1",
+        text: "custom tag 1"
+      }
     }
   end
 
   post "/gifs/gif2/tags", { tag: "Shared Tag" } do
     status 201
     {
-      id: "shared-tag",
-      text: "shared tag"
+      tag: {
+        id: "shared-tag",
+        text: "shared tag"
+      }
     }
   end
 
   post "/gifs/gif2/tags", { tag: "Custom Tag 2" } do
     status 201
     {
-      id: "custom-tag-2",
-      text: "custom tag 2"
+      tag: {
+        id: "custom-tag-2",
+        text: "custom tag 2"
+      }
     }
   end
 
   get "/tags" do
     status 200
-    [{
-      id: "custom-tag-2",
-      text: "custom tag 2"
-    }, {
-      id: "custom-tag-1",
-      text: "custom tag 1"
-    }, {
-      id: "shared-tag",
-      text: "shared tag"
-    }]
-  end
-
-  get "/gifs/gif1/tags" do
-    status 200
-    [{
-      id: "custom-tag-1",
-      text: "custom tag 1"
-    }, {
-      id: "shared-tag",
-      text: "shared tag"
-    }]
-  end
-
-  get "/gifs/gif2/tags" do
-    status 200
-    [{
-      id: "custom-tag-2",
-      text: "custom tag 2"
-    }, {
-      id: "shared-tag",
-      text: "shared tag"
-    }]
-  end
-
-  get "/tags?q=shared" do
-    status 200
-    [{
-      id: "shared-tag",
-      text: "shared tag"
-    }]
-  end
-
-  get "/tags/complete?q=cust" do
-    status 200
-    [{
-      id: "custom-tag-2",
-      text: "custom tag 2"
-    }, {
-      id: "custom-tag-1",
-      text: "custom tag 1"
-    }]
-  end
-
-  delete "/gifs/gif1/tags/custom-tag-1" do
-    status 202
     {
-      id: "custom-tag-1",
-      text: "custom tag 1"
+      tags: [{
+        id: "custom-tag-2",
+        text: "custom tag 2"
+      }, {
+        id: "custom-tag-1",
+        text: "custom tag 1"
+      }, {
+        id: "shared-tag",
+        text: "shared tag"
+      }]
     }
   end
 
   get "/gifs/gif1/tags" do
     status 200
-    [{
-      id: "shared-tag",
-      text: "shared tag"
-    }]
+    {
+      tags: [{
+        id: "custom-tag-1",
+        text: "custom tag 1"
+      }, {
+        id: "shared-tag",
+        text: "shared tag"
+      }]
+    }
+  end
+
+  get "/gifs/gif2/tags" do
+    status 200
+    {
+      tags: [{
+        id: "custom-tag-2",
+        text: "custom tag 2"
+      }, {
+        id: "shared-tag",
+        text: "shared tag"
+      }]
+    }
+  end
+
+  get "/tags?q=shared" do
+    status 200
+    {
+      tags:
+      [{
+        id: "shared-tag",
+        text: "shared tag"
+      }]
+    }
+  end
+
+  get "/tags/complete?q=cust" do
+    status 200
+    {
+      tags:[{
+        id: "custom-tag-2",
+        text: "custom tag 2"
+      }, {
+        id: "custom-tag-1",
+        text: "custom tag 1"
+      }]
+    }
+  end
+
+  delete "/gifs/gif1/tags/custom-tag-1" do
+    status 202
+    {
+      tag: {
+        id: "custom-tag-1",
+        text: "custom tag 1"
+      }
+    }
+  end
+
+  get "/gifs/gif1/tags" do
+    status 200
+    {
+      tags: [{
+        id: "shared-tag",
+        text: "shared tag"
+      }]
+    }
   end
 
   get "/gifs?q=shared" do
     status 200
-    [{
-      id: "gif2",
-      url: "http://i.imgur.com/gif2.gif",
-      thumbnail_url: "http://i.imgur.com/gif2b.gif"
-    }, {
-      id: "gif1",
-      url: "http://i.imgur.com/gif1.gif",
-      thumbnail_url: "http://i.imgur.com/gif1b.gif"
-    }]
+    {
+      gifs: [{
+        id: "gif2",
+        url: "http://i.imgur.com/gif2.gif",
+        thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      }, {
+        id: "gif1",
+        url: "http://i.imgur.com/gif1.gif",
+        thumbnail_url: "http://i.imgur.com/gif1b.gif"
+      }]
+    }
   end
 
   get "/gifs?q=custom" do
     status 200
-    [{
-      id: "gif2",
-      url: "http://i.imgur.com/gif2.gif",
-      thumbnail_url: "http://i.imgur.com/gif2b.gif"
-    }]
+    {
+      gifs: [{
+        id: "gif2",
+        url: "http://i.imgur.com/gif2.gif",
+        thumbnail_url: "http://i.imgur.com/gif2b.gif"
+      }]
+    }
   end
 end
 
